5737394d83b5cd0be3180ec8b6ff927e651a5035
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-08-13 13:12:42 +0200

    remove react

--- src/board.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Board() {
-  return (
-    <div className="board">
-      <button>Play</button>
-    </div>
-  )
-}
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import Cloudboard from './cloudboard'
-
-ReactDOM.render(<Cloudboard/>, document.getElementById('root'))
