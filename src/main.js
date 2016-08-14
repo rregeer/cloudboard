c63ec9ce52e7193da693ccd38b20c1d154195ad3
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-08-14 16:53:10 +0200

    Add eslint and split bootstrap file

--- src/main.js
+++ src/main.js
@@ -1,40 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-import { Router, Route, browserHistory } from 'react-router'
 import { Style } from 'radium'
-import createLogger from 'redux-logger';
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
 
-import collections from '../etc/sound-collections.json'
-import db from './db'
-import { normalizeSounds } from './helpers'
+import store from './store'
 import Cloudboard from './components/cloudboard'
-import queueReducer from './reducer';
 import baseStyles from './styles/base'
 
-import SoundEventRepository from './sound-event-repository'
-import createQueueMiddleware from './middleware/queue-middleware'
-import playerMiddleware from './middleware/player-middleware'
-
-const soundEventRepository = new SoundEventRepository(db)
-const sounds = normalizeSounds(collections)
-const queueMiddleware = createQueueMiddleware(soundEventRepository, sounds)
-
-let middlewares = [playerMiddleware, queueMiddleware]
-const reducer = combineReducers({
-  queue: queueReducer,
-  routing: routerReducer,
-  collections: () => collections,
-  sounds: () => sounds
-})
-
-if (process.env.NODE_ENV === 'development') {
-  middlewares = [...middlewares, createLogger()]
-}
-
-const store = createStore(reducer, applyMiddleware(...middlewares))
 const history = syncHistoryWithStore(browserHistory, store)
 const rootEl = document.getElementById('root')
 
---
+++ src/store.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+import createLogger from 'redux-logger'
+
+import collections from '../etc/sound-collections.json'
+import db from './db'
+import { normalizeSounds } from './helpers'
+import queueReducer from './reducer'
+import SoundEventRepository from './sound-event-repository'
+import createQueueMiddleware from './middleware/queue-middleware'
+import playerMiddleware from './middleware/player-middleware'
+
+const soundEventRepository = new SoundEventRepository(db)
+const sounds = normalizeSounds(collections)
+const queueMiddleware = createQueueMiddleware(soundEventRepository, sounds)
+
+let middlewares = [playerMiddleware, queueMiddleware]
+const reducer = combineReducers({
+  queue: queueReducer,
+  routing: routerReducer,
+  collections: () => collections,
+  sounds: () => sounds
+})
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares = [...middlewares, createLogger()]
+}
+
+export default createStore(reducer, applyMiddleware(...middlewares))
