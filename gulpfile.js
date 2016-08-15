d4c87ab301217aa77ed2d50de27a0ba5409d1d33
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-08-15 17:04:31 +0200

    Add deployment step

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp')
+const ftp = require('vinyl-ftp')
+const minimist = require('minimist')
+const util = require('gulp-util')
+
+const args = minimist(process.argv.slice(2))
+
+gulp.task('deploy', () => {
+  const remotePath = '/public_html/nielsgerritsen/cloudboard'
+  const conn = ftp.create({
+    host: 'carehr.nl',
+    user: args.user,
+    pass: args.password,
+    log: util.log
+  })
+
+  return gulp.src(['./public/**', './sounds/**/*.mp3', './index.html'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath))
+})
